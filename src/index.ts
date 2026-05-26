227f62f9f227f45945c64021ae42d905dbc18a82
suhanisagar2410 <suhaniisagar33@gmail.com>
2026-05-27 00:12:01 +0530

    cors update

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,11 @@ import { uploadReplayData, fetchReplayData } from './storage/s3';
 const prisma = new PrismaClient();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key']
+}));
 app.use(express.json({ limit: '100mb' }));
 
 import authRouter from './api/auth';
